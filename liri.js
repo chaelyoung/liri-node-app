9cd6ef0bdd548daba2b597161534ac64b3a8ca87
Mike Young <chaelyoung>
2018-05-18 21:19:10 -0500

    updates files

--- liri.js
+++ liri.js
@@ -1,4 +1,13 @@
 require("dotenv").config();
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+//var spotify = new Spotify(keys.spotify);
+//var client = new Twitter(keys.twitter);
+
+var Twitter = require("twitter");
+var Spotify = require("spotify");
+var request = require("request");
+var fs = require("fs");
+
+var keys = require("./keys.js");
+var twitterKeys = keys.twitterKeys;
+
